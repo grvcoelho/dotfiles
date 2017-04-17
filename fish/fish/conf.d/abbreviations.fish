0bde81bf7289a0a6ec59057bfb918f3e480b7659
Guilherme Coelho <guilhermervcoelho@gmail.com>
2017-04-16 21:36:56 -0300

    fish: add tmux abbreviations

--- fish/fish/conf.d/abbreviations.fish
+++ fish/fish/conf.d/abbreviations.fish
@@ -7,4 +7,8 @@ abbr gf 'git fetch'
 abbr gl 'git log'
 abbr gll 'git log --oneline'
 
-
+# Tmux abbreviations
+abbr ta 'tmux attach -t'
+abbr tl 'tmux list-sessions'
+abbr ts 'tmux new-session -s'
+abbr tkss 'tmux kill-session -t'
