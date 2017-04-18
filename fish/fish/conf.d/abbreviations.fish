37a38f1c3bebceb142c1c44e9b3416ac64a65862
Guilherme Coelho <guilhermervcoelho@gmail.com>
2017-04-18 08:39:19 -0300

    abbr: git pull and git status

--- fish/fish/conf.d/abbreviations.fish
+++ fish/fish/conf.d/abbreviations.fish
@@ -1,8 +1,10 @@
 # Git abbreviations
 abbr ga 'git add'
+abbr gs 'git status'
 abbr gc 'git commit'
 abbr gco 'git checkout'
 abbr gp 'git push'
+abbr gpl 'git pull'
 abbr gf 'git fetch'
 abbr gl 'git log'
 abbr gll 'git log --graph --decorate --pretty=oneline --abbrev-commit'
