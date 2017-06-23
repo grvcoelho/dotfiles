8d73e38ee7a275989444df7684e0f2e4e99c0709
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2017-06-23 12:03:59 -0300

    z: move z fish configuration

--- fish/fish/config.fish
+++ fish/fish/config.fish
@@ -3,9 +3,6 @@ if status --is-interactive
   eval sh $HOME/.config/base16-shell/scripts/base16-gruvbox-dark-hard.sh
 end
 
-# Set z plugin path
-set -gx Z_SCRIPT_PATH /usr/local/etc/profile.d/z.sh
-
 # Set go path
 set -x GOPATH $HOME/go
 set -x GOBIN $GOPATH/bin
