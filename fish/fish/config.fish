cfd2454db7ecad9bb426fb67ca4fb376aec3d525
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2017-12-15 13:09:21 -0200

    fish: change theme to dracula

--- fish/fish/config.fish
+++ fish/fish/config.fish
@@ -1,6 +1,6 @@
 # Set base16-shell theme
 if status --is-interactive
-  eval sh $HOME/.config/base16-shell/scripts/base16-material-darker.sh
+  eval sh $HOME/.config/base16-shell/scripts/base16-dracula.sh
 end
 
 # Set go path
