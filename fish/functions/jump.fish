487e13b9f75dc9cab02ec1c436d20b9c16e63f8d
Guilherme Coelho <guilhermervcoelho@gmail.com>
2019-12-28 17:24:39 -0300

    fish: improve jump function

--- fish/functions/jump.fish
+++ fish/functions/jump.fish
@@ -1,10 +1,37 @@
 function jump
-	set project $argv[1]
-    set dir ~/Workspace/$project
+	set -l target $argv[1]
+  set -l workspace_dir ~/Workspace
+  set -l parts (string split "/" $target)
+
+  if test -z $target
+    cd $workspace_dir
+    return
+  end
+
+  if test -z $parts[2]
+    set -l dir $workspace_dir/$parts[1]
 
     if not test -d $dir
-        git clone git@github.com:$project $dir
+      mkdir -p $dir
     end
 
     cd $dir
+
+    return
+  end
+
+  if test -n $parts[2]
+    set dir $workspace_dir/$target
+
+    if not test -d $dir
+      mkdir -p $dir
+      git clone git@github.com:$target $dir
+      cd $dir
+      git remote remove origin
+      git remote add origin git@github.com:$target
+    end
+
+    cd $dir
+    return
+  end
 end
